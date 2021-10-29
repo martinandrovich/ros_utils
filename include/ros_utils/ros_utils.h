334e626714262e6240dfd07bfad69969a225ec85
Martin <martinandrovich@gmail.com>
2021-10-29 12:20:58 +0200

    include everything

--- include/ros_utils/ros_utils.h
+++ include/ros_utils/ros_utils.h
@@ -1,3 +1,9 @@
 #pragma once
 
+// include everything
 #include <ros_utils/ros.h>
+#include <ros_utils/gazebo.h>
+#include <ros_utils/moveit.h>
+#include <ros_utils/geometry_msgs.h>
+#include <ros_utils/std.h>
+#include <ros_utils/math.h>
