2550a4dff64e115cd2e99e2f7f240cb468372611
Martin <martinandrovich@gmail.com>
2021-11-18 11:39:02 +0100

    housekeeping

--- include/ros_utils/eigen.h
+++ include/ros_utils/eigen.h
@@ -10,10 +10,10 @@ namespace Eigen
 
 	Eigen::Isometry3d
 	make_tf(const std::array<double, 3>& pos, const Eigen::Vector3d& axis, double theta);
-	
+
 	// Eigen::Isometry3d // todo
 	// make_tf(const std::array<double, 3>& pos, const std::array<double, 3>& axis, double theta);
-	
+
 	Eigen::Isometry3d
 	make_tf(const std::array<double, 3>& pos, const std::array<double, 3>& rpy = {0, 0, 0});
 
--- include/ros_utils/ros_utils.h
+++ include/ros_utils/ros_utils.h
@@ -1,9 +1,10 @@
 #pragma once
 
 // include everything
-#include <ros_utils/ros.h>
+#include <ros_utils/eigen.h>
 #include <ros_utils/gazebo.h>
-#include <ros_utils/moveit.h>
 #include <ros_utils/geometry_msgs.h>
-#include <ros_utils/std.h>
 #include <ros_utils/math.h>
+#include <ros_utils/moveit.h>
+#include <ros_utils/ros.h>
+#include <ros_utils/std.h>
